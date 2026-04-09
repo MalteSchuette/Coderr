9c752c2bf67e3d89b2be45c17e253ea9c69436e7
Malte <malte.schuette92@gmail.com>
2026-04-09 16:06:36 +0200

    Update migration timestamps and refactor user-related serializers and models

--- users_app/api/serializers.py
+++ users_app/api/serializers.py
@@ -1,31 +1,29 @@
 from rest_framework import serializers
-from ..models import Profile
+from ..models import CustomUser
+
+
+
+
+class UserSerializer(serializers.ModelSerializer):
+    user_id = serializers.IntegerField(source='id', read_only=True)
+    token = serializers.SerializerMethodField()
+
+    def get_token(self, obj):
+        from rest_framework.authtoken.models import Token
+        token, _ = Token.objects.get_or_create(user=obj)
+        return token.key
+
+    class Meta:
+        model = CustomUser
+        fields = ['token', 'username', 'email', 'user_id']
+
 
 class ProfileSerializer(serializers.ModelSerializer):
 
-    username = serializers.SerializerMethodField()
-    first_name = serializers.SerializerMethodField()
-    last_name = serializers.SerializerMethodField()
-    email = serializers.SerializerMethodField()
-    created_at = serializers.SerializerMethodField()
-    type = serializers.CharField(source='profile_type', read_only=True)
-    
 
-    def get_username(self, obj):
-        return obj.user.username
-    
-    def get_first_name(self, obj):
-        return obj.user.first_name
-    
-    def get_last_name(self, obj):
-        return obj.user.last_name
-    
-    def get_email(self, obj):
-        return obj.user.email
-    
-    def get_created_at(self, obj):
-        return obj.user.date_joined
+    type = serializers.CharField(source='profile_type', read_only=True)
+    created_at = serializers.DateTimeField(source='date_joined', read_only=True)
 
     class Meta:
-        model = Profile
-        fields = ['user', 'username', 'first_name', 'last_name', 'file', 'location', 'tel', 'description', 'working_hours', 'type', 'email', 'created_at',]
+        model = CustomUser
+        fields = ['username', 'first_name', 'last_name', 'file', 'location', 'tel', 'description', 'working_hours', 'type', 'email', 'created_at']
--- users_app/models.py
+++ users_app/models.py
@@ -1,25 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
-
 TYPE_CHOICES = [
     ('customer', 'Customer'),
     ('business', 'Business'),
 ]
 
 class CustomUser(AbstractUser):
-    profile_type = models.CharField(max_length=20, choices=TYPE_CHOICES)
-    
-class Profile(models.Model):
-    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    profile_type = models.CharField(max_length=20, choices=TYPE_CHOICES, blank=True)
     file = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
     location = models.CharField(max_length=30, blank=True)
     tel = models.CharField(max_length=20, blank=True)
     description = models.TextField(blank=True)
-    working_hours = models.CharField(max_length=30,blank=True)
-
+    working_hours = models.CharField(max_length=30, blank=True)
 
     def __str__(self):
-        return self.user.username
+        return self.username
 
