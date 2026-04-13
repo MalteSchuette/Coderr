61be937938089b045df03a99711c3c66c587e9d3
Malte <malte.schuette92@gmail.com>
2026-04-13 16:29:09 +0200

    Add serializers for offers, offer details, and user information

--- offers_app/api/serializers.py
+++ offers_app/api/serializers.py
@@ -1,7 +1,59 @@
 from rest_framework import serializers
-from ..models import Offer
+from ..models import Offer, OfferDetail
+from django.contrib.auth import get_user_model
 
-class OfferSerializer(serializers.ModelSerializer):
+User = get_user_model()
+
+
+class OfferUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username']
+
+
+class OfferDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OfferDetail
+        fields = ['id', 'title', 'revisions', 'delivery_time_in_days', 'price', 'features', 'offer_type']
+
+
+class OfferDetailMinimalSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='offerdetail-detail')
+
+    class Meta:
+        model = OfferDetail
+        fields = ['id', 'url']
+
+
+class OfferListSerializer(serializers.ModelSerializer):
+    details = OfferDetailMinimalSerializer(many=True, read_only=True, source='offer_details')
+    min_price = serializers.SerializerMethodField()
+    min_delivery_time = serializers.SerializerMethodField()
+    user_details = OfferUserSerializer(source='user', read_only=True)
+
+    def get_min_price(self, obj):
+        return obj.offer_details.order_by('price').first().price
+
+    def get_min_delivery_time(self, obj):
+        return obj.offer_details.order_by('delivery_time_in_days').first().delivery_time_in_days
+
     class Meta:
         model = Offer
-        fields = []
+        fields = ['id', 'user', 'title', 'image', 'description', 'created_at', 'updated_at', 'details', 'min_price', 'min_delivery_time', 'user_details']
+
+
+class OfferRetrieveSerializer(serializers.ModelSerializer):
+    details = OfferDetailSerializer(many=True, read_only=True, source='offer_details')
+    min_price = serializers.SerializerMethodField()
+    min_delivery_time = serializers.SerializerMethodField()
+    user_details = OfferUserSerializer(source='user', read_only=True)
+
+    def get_min_price(self, obj):
+        return obj.offer_details.order_by('price').first().price
+
+    def get_min_delivery_time(self, obj):
+        return obj.offer_details.order_by('delivery_time_in_days').first().delivery_time_in_days
+
+    class Meta:
+        model = Offer
+        fields = ['id', 'user', 'title', 'image', 'description', 'created_at', 'updated_at', 'details', 'min_price', 'min_delivery_time', 'user_details']
