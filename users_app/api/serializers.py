1c39f067eac07a4570a0355f651214240ea62db7
Malte <malte.schuette92@gmail.com>
2026-04-15 13:46:06 +0200

    Enhance user registration and login functionality with token generation and validation

--- users_app/api/serializers.py
+++ users_app/api/serializers.py
@@ -5,15 +5,29 @@ from ..models import CustomUser
 class UserSerializer(serializers.ModelSerializer):
     user_id = serializers.IntegerField(source='id', read_only=True)
     token = serializers.SerializerMethodField()
+    repeated_password = serializers.CharField(write_only=True)
 
     def get_token(self, obj):
         from rest_framework.authtoken.models import Token
         token, _ = Token.objects.get_or_create(user=obj)
         return token.key
 
+    def validate(self, data):
+        if data['password'] != data.pop('repeated_password'):
+            raise serializers.ValidationError("Passwörter stimmen nicht überein!")
+        return data
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(**validated_data)
+        return user
+
     class Meta:
         model = CustomUser
-        fields = ['token', 'username', 'email', 'user_id']
+        fields = ['token', 'username', 'email', 'user_id', 'password', 'repeated_password', 'profile_type']
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'profile_type': {'write_only': False}
+        }
 
 
 class ProfileSerializer(serializers.ModelSerializer):
--- users_app/api/views.py
+++ users_app/api/views.py
@@ -1,5 +1,50 @@
 from rest_framework import generics
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.response import Response
+from ..models import CustomUser
+from .serializers import UserSerializer, ProfileSerializer
 
-class UserList(generics.ListAPIView):
-    pass
+
+class RegistrationView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+
+    def perform_create(self, serializer):
+        user = serializer.save()
+        Token.objects.create(user=user)
+
+
+class LoginView(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'username': user.username,
+            'email': user.email,
+            'user_id': user.id
+        })
+    
+
+class ProfileDetailView(generics.RetrieveUpdateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = ProfileSerializer
+
+
+class BusinessProfilesView(generics.ListAPIView):
+    serializer_class = ProfileSerializer
+
+    def get_queryset(self):
+        return CustomUser.objects.filter(profile_type='business')
+
+
+class CustomerProfilesView(generics.ListAPIView):
+    serializer_class = ProfileSerializer
+
+    def get_queryset(self):
+        return CustomUser.objects.filter(profile_type='customer')
+
 
